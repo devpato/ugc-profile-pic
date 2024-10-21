d799a457730b5283adc9273ce39bdccc5e59c8de
Caroline Levison <caroline.levison@cloudinary.com>
2024-10-21 17:13:56 +0100

    Remove useEffect

--- app/context/UserContext.tsx
+++ app/context/UserContext.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { createContext, useState, useContext, useEffect } from 'react'
+import React, { createContext, useState, useContext } from 'react'
 
 interface Post {
   id: number
